3388c7ef87b91283f7c79882de8e90d56957fe66
ferueda <fe.rueda@outlook.com>
2023-03-14 21:53:23 -0700

    Auth: add express-async-errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
