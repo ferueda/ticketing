9807ed32c0262d7ec31652aae8720202d034a048
ferueda <fe.rueda@outlook.com>
2023-03-15 21:04:53 -0700

    Auth: add password hashing to user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // Describes properties required
 // to create a new user
@@ -32,6 +33,14 @@ const userSchema = new mongoose.Schema({
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
 
 const User = mongoose.model<UserDoc, UserModel>('user', userSchema);
 
