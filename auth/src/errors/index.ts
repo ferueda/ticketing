da41ea086f76b43deac7076ba5f35df64cc7351d
ferueda <fe.rueda@outlook.com>
2023-03-15 21:04:38 -0700

    Auth: add bad-request-error

--- auth/src/errors/index.ts
+++ auth/src/errors/index.ts
@@ -1,5 +1,6 @@
 import { DatabaseConnError } from './database-conn-error';
 import { RequestValidationError } from './request-validation-error';
 import { NotFoundError } from './not-found-error';
+import { BadRequestError } from './bad-request-error';
 
-export { DatabaseConnError, RequestValidationError, NotFoundError };
+export { DatabaseConnError, RequestValidationError, NotFoundError, BadRequestError };
