de1250b33f3779796d6c5bef84da8996f04c0ac5
ferueda <fe.rueda@outlook.com>
2023-03-15 21:06:02 -0700

    Auth: update main app

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,12 +2,38 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 
+import mongoose from 'mongoose';
+
+import { signupRouter, signinRouter, signoutRouter, currentUserRouter } from './routes';
 import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors';
+
 const app = express();
+
 app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000');
+  });
+};
+
+start();
