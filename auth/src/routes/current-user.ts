b1c5005fcc73ae34a0ed3f796b4f5b54b4ca308f
ferueda <fe.rueda@outlook.com>
2023-03-17 20:19:33 -0700

    Auth: add current-user middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,16 +1,12 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+
+import { currentUser } from '../middleware/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
+router.get('/api/users/currentuser', currentUser, (req, res) => {
   if (!req.session?.jwt) return res.send({ user: null });
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ user: payload });
-  } catch (err) {
-    return res.send({ user: null });
-  }
+  res.send({ user: req.user || null });
 });
 
 export { router as currentUserRouter };
