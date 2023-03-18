e85b7a497fa80c251535de08226d31802958128f
ferueda <fe.rueda@outlook.com>
2023-03-17 19:23:46 -0700

    Auth: Add toJSON user model modifier

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -19,16 +19,28 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
+  {
+    toJSON: {
+      transform(_, ret) {
+        ret.id = ret._id;
+        delete ret.password;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
   },
-});
+);
 
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
