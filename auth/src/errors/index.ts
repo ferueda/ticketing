f836d62fc12cc7137bf995e12075f89d3fac9d58
ferueda <fe.rueda@outlook.com>
2023-03-17 20:20:22 -0700

    Auth: hupdate imports

--- auth/src/errors/index.ts
+++ auth/src/errors/index.ts
@@ -2,5 +2,12 @@ import { DatabaseConnError } from './database-conn-error';
 import { RequestValidationError } from './request-validation-error';
 import { NotFoundError } from './not-found-error';
 import { BadRequestError } from './bad-request-error';
+import { NotAuthorizedError } from './not-authorized-error';
 
-export { DatabaseConnError, RequestValidationError, NotFoundError, BadRequestError };
+export {
+  DatabaseConnError,
+  RequestValidationError,
+  NotFoundError,
+  BadRequestError,
+  NotAuthorizedError,
+};
