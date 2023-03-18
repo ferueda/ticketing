ccdab8121c0893cebc3cdbe1e8f636e7a7e95ae6
ferueda <fe.rueda@outlook.com>
2023-03-17 19:54:23 -0700

    Auth: implement currentuser route logic

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+
+const router = express.Router();
+
+router.get('/api/users/currentuser', (req, res) => {
+  if (!req.session?.jwt) return res.send({ user: null });
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ user: payload });
+  } catch (err) {
+    return res.send({ user: null });
+  }
+});
+
+export { router as currentUserRouter };
