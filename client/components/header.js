93ecc7f801c243fea48f0851514bce46cb7ff27a
ferueda <fe.rueda@outlook.com>
2023-03-19 00:23:15 -0700

    Client: create header component

---
+++ client/components/header.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+export default ({ user }) => {
+  const links = [
+    !user && { label: 'Sign Up', href: '/auth/signup' },
+    !user && { label: 'Sign In', href: '/auth/signin' },
+    user && { label: 'Sign Out', href: '/auth/signout' },
+  ]
+    .filter(Boolean)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <Link href={href} legacyBehavior>
+            <a className="nav-link">{label}</a>
+          </Link>
+        </li>
+      );
+    });
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <Link href="/" legacyBehavior>
+        <a className="navbar-brand">Ticketing</a>
+      </Link>
+
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </nav>
+  );
+};
