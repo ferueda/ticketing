c03bcb2ee81ef58b794d739f501e63dc1fd29cb7
ferueda <fe.rueda@outlook.com>
2023-03-19 10:38:23 -0700

    Auth: Move mongo uri to env

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,8 +4,10 @@ import { app } from './app';
 
 const start = async (app: Express) => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY is not defined');
+  if (!process.env.MONGO_URI) throw new Error('MONGO_URI is not defined');
+
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI!);
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error(err);
