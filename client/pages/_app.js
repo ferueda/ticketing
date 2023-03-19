40fd4332bdab3401a653ba57ed21a06799804a2d
ferueda <fe.rueda@outlook.com>
2023-03-19 00:23:35 -0700

    Client: configure _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/buildClient';
+import Header from '../components/header';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, user }) => {
+  return (
+    <div>
+      <Header user={user} />
+      <Component {...pageProps} />;
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser').catch((err) => {
+    console.log(err);
+  });
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return { pageProps, user: data.user };
+};
+
+export default AppComponent;
