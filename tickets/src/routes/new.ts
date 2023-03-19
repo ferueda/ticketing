617ca994665508a4502601622f7a9c8cdfa1b085
ferueda <fe.rueda@outlook.com>
2023-03-19 12:18:43 -0700

    Tickets: add new tickets endpoint

---
+++ tickets/src/routes/new.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@frticketing/common';
+
+import { Ticket } from '../models/tickets';
+
+const router = express.Router();
+
+router.post(
+  '/api/tickets',
+  requireAuth,
+  body('title').notEmpty().withMessage('title is invalid'),
+  body('price').isFloat({ gt: 0 }).withMessage('price must be greater than 0'),
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+    const ticket = Ticket.build({ title, price, userId: req.user!.id });
+    await ticket.save();
+    res.status(201).send(ticket);
+  },
+);
+
+export { router as createTicketRouter };
