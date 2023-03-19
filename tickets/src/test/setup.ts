cdb1531127515187ad6cb719535ca9d79a3ffdbc
ferueda <fe.rueda@outlook.com>
2023-03-19 12:19:05 -0700

    Tickets: Update test setup file

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: MongoMemoryServer;
@@ -28,12 +28,11 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'PassworD!';
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({ email, password })
-    .expect(201);
-  return response.get('Set-Cookie');
+global.signin = () => {
+  const payload = { id: 'abc123', email: 'test@test.com' };
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+  return [`session=${base64}`];
 };
