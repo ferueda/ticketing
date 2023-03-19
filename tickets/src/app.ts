82429ac20544c780d9d671ddbc0a5d86e88efd18
ferueda <fe.rueda@outlook.com>
2023-03-19 12:19:41 -0700

    Ticket: Update app and routes

---
+++ tickets/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@frticketing/common';
+import { currentUser } from '@frticketing/common';
+
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+
+export const app = express();
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
