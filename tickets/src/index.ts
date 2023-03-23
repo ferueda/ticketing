3cf3a2f3285860774b0412712959794008992ea8
ferueda <fe.rueda@outlook.com>
2023-03-22 23:56:05 -0700

    Tickets: wire NATS to the application

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,12 +1,22 @@
 import mongoose from 'mongoose';
 import { Express } from 'express';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async (app: Express) => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY is not defined');
   if (!process.env.MONGO_URI) throw new Error('MONGO_URI is not defined');
 
   try {
+    await natsWrapper.connect('ticketing', 'sdfdsf', 'http://nats-srv:4222');
+    this;
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI!);
     console.log('Connected to MongoDB');
   } catch (err) {
