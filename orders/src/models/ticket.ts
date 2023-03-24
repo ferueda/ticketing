b57da55e5af2412f8b6b1d093e8d14ea74d9ace8
ferueda <fe.rueda@outlook.com>
2023-03-23 23:21:27 -0700

    Orders: Add ticket model

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+// Describes properties required
+// to create a new ticket
+interface TicketAttrs {
+  title: string;
+  price: number;
+}
+
+// Describes the properties a Ticket document has
+export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+}
+
+// Describes properties the Ticket model has
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  },
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
