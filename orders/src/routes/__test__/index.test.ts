775b07c027ecab596b690aa75042578b3a2af014
ferueda <fe.rueda@outlook.com>
2023-03-26 23:24:13 -0700

    Orders: Add index test suite

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,39 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+it('has a route handler listening to /api/orders for get requests', async () => {
+  const response = await request(app).get('/api/orders').send({});
+  expect(response.status).not.toEqual(404);
+});
+
+it('can only be accessed if user is signed in', async () => {
+  const response = await request(app).get('/api/orders').send({});
+  expect(response.status).toEqual(401);
+});
+
+it('can fetch a list of orders', async () => {
+  const tickets = [
+    { title: 'test 1', price: 10 },
+    { title: 'test 2', price: 20 },
+  ];
+
+  for (let ticket of tickets) {
+    await Ticket.build(ticket).save();
+  }
+  const cookie = global.signin();
+  const firstResponse = await request(app).get('/api/orders').set('Cookie', cookie).send();
+  expect(firstResponse.status).toEqual(200);
+  expect(firstResponse.body.length).toEqual(0);
+
+  const createdTickets = await Ticket.find({});
+
+  for (let ticket of createdTickets) {
+    await request(app).post('/api/orders').set('Cookie', cookie).send({ ticketId: ticket.id });
+  }
+
+  const secondResponse = await request(app).get('/api/orders').set('Cookie', cookie).send();
+
+  expect(secondResponse.status).toEqual(200);
+  expect(secondResponse.body.length).toEqual(2);
+});
