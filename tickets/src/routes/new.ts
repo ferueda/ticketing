c229c86820700368819e23e7135c1adf28bafddb
ferueda <fe.rueda@outlook.com>
2023-04-09 22:12:52 -0700

    Tickets: Update routes

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -22,6 +22,7 @@ router.post(
     await ticket.save();
     await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
+      version: ticket.version,
       price: ticket.price,
       title: ticket.title,
       userId: ticket.userId,
