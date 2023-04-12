e761ec113ec1d222e53310ebaaf4984ad758a6b8
ferueda <fe.rueda@outlook.com>
2023-04-11 19:22:59 -0700

    Tickets: extend update route tests

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 import { natsWrapper } from '../../nats-wrapper';
 
 it('returns a 404 if the provided id does not exist', async () => {
@@ -91,7 +92,7 @@ it('updates the ticket provided valid inputs', async () => {
   const newTitle = 'updated';
   const newPrice = 1000;
 
-  const r = await request(app)
+  await request(app)
     .put(`/api/tickets/${response.body.id}`)
     .set('Cookie', userCookie)
     .send({
@@ -116,7 +117,7 @@ it('updates the ticket provided valid inputs', async () => {
   const newTitle = 'updated';
   const newPrice = 1000;
 
-  const r = await request(app)
+  await request(app)
     .put(`/api/tickets/${response.body.id}`)
     .set('Cookie', userCookie)
     .send({
@@ -126,4 +127,25 @@ it('updates the ticket provided valid inputs', async () => {
     .expect(200);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('rejects updates if ticket is reserved', async () => {
+  const userCookie = global.signin();
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', userCookie)
+    .send({ title: 'test', price: 20 });
+
+  const ticket = await Ticket.findById(response.body.id);
+  ticket!.set({ orderId: new mongoose.Types.ObjectId().toHexString() });
+  await ticket!.save();
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', userCookie)
+    .send({
+      title: 'newTitle',
+      price: 20,
+    })
+    .expect(400);
 });
