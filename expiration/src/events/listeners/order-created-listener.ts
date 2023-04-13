578e8d49378a40bcff62c8cf51bc7519dd119633
ferueda <fe.rueda@outlook.com>
2023-04-12 22:32:10 -0700

    Expiration: create order-created-listener

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,22 @@
+import { Listener, OrderCreatedEvent, Subject } from '@frticketing/common';
+import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queues/expiration-queue';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subject.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log(delay);
+    await expirationQueue.add(
+      { orderId: data.id },
+      {
+        delay,
+      },
+    );
+
+    msg.ack();
+  }
+}
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,9 +1,11 @@
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.NATS_CLIENT_ID) throw new Error('NATS_CLIENT_ID is not defined');
   if (!process.env.NATS_URL) throw new Error('NATS_URL is not defined');
   if (!process.env.NATS_CLUSTER_ID) throw new Error('NATS_CLUSTER_ID is not defined');
+  if (!process.env.REDIS_HOST) throw new Error('REDIS_HOST is not defined');
 
   try {
     await natsWrapper.connect(
@@ -18,6 +20,8 @@ const start = async () => {
     });
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
   } catch (err) {
     console.error(err);
   }
